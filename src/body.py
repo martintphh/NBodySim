79dafb4f32800467fbb8bac1756395531634a16a
martintphh <103767844+martintphh@users.noreply.github.com>
2025-05-22 22:53:09 +0200

    Added simulation(Euler-Method), 3d animation, 3d plot

--- src/body.py
+++ src/body.py
@@ -5,14 +5,14 @@ class Body:
     def __init__(self, name, mass, position, velocity):
         self.name = name #str
         self.mass = mass #float
-        self.position = position #ndarray 3D
-        self.velocity = velocity #ndarray 3D
+        self.position = np.array(position, dtype=float) #ndarray 3D
+        self.velocity = np.array(velocity, dtype=float) #ndarray 3D
         self.force = np.zeros(3) #can be updated later
 
     def __repr__(self):
         return f"Body {self.name}: position={self.position}, velocity={self.velocity}"
 
-    def force_from(self, other_body, G = 4 * np.pi**2):
+    def force_from(self, other_body, G = 6.6743e-11):
         """force from other_body on self
         units AU, where G=4pi^2"""
 
@@ -22,4 +22,5 @@ class Body:
             return np.zeros(0)
         force_magnitude = G * self.mass * other_body.mass / r_magnitude**2
         force_vec = force_magnitude * r_vec / r_magnitude
+
         return force_vec
--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from body import Body
+from simulation import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # aktiviert 3D-Plotting
+
 
 
 def main():
-    earth = Body("Earth", 6, np.array([1,2,3]), [0,0,0])
-    sun = Body("Sun", 100000, np.array([1,2,13]), [0,0,0])
-    print(earth.force_from(sun))
-    print(earth)
+    earth = Body(
+        name="Earth",
+        mass =5.972e24,
+        position=np.array([1.496e11,0.,0.]),
+        velocity=np.array([0.,29780.,0.]))
+
+    sun = Body("Sun", 
+    mass=1.9885e30,
+    position=np.array([0,0,0]),
+    velocity=np.array([0.,0.,0.]))
+    
+    jupyter = Body("Jupyter",
+    mass=1.898e27,
+    position=np.array([7.758e11,0,0]),
+    velocity=np.array([0,13058,0]))
+
+
+    bodies = [sun, earth, jupyter]
+
+    dt = 60 * 60 *24 #1h in s
+    steps = 365 * 24 #ein Jahr
+
+    positions = simulate(bodies, steps, dt)
+    
+    #plot_trajectories_3d(bodies, positions)
+
+    animation(bodies, positions)
 
 
     return 0
